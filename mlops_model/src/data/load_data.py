f4361d63284f5d02ed648fa4f5c5b654f5f131df
Ramesh-Karnati <ramesh.kv1989@gmail.com>
2022-12-15 06:26:32 +0000

    New Updates

---
+++ mlops_model/src/data/load_data.py
@@ -0,0 +1,43 @@
+import yaml
+import argparse
+import pandas as pd
+
+def read_params(config_path):
+    """
+    read parameters from params.yaml file
+    input: params.yaml location
+    output: paramters as dictionary
+    """
+    with open(config_path) as yaml_file:
+        config=yaml.safe_load(yaml_file)
+    return config
+
+def load_data(data_path,selected_features):
+    """
+    load csv data from given path
+    input: csv path
+    output: pandas dataframe with only selected features from config file based on 
+    prototype code knowledge.   
+    """
+    df = pd.read_csv(data_path,sep=",",encoding='utf-8')
+    df = df[selected_features]
+    return df
+
+def load_raw_data(config_path):
+    """
+    load data from location(data/external) to (data/raw) location
+    input: config_path
+    output: saves the file to data/raw folder
+    """
+    config = read_params(config_path)
+    external_data_path = config["external_data_config"]["external_data_csv"]
+    raw_data_path = config["raw_data_config"]["raw_data_csv"]
+    selected_features=config["raw_data_config"]["model_var"]
+    df = load_data(external_data_path,selected_features)
+    df.to_csv(raw_data_path,index=False)
+
+if __name__=='__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args = args.parsed_args()
+    load_raw_data(config_path = parsed_args.config)
